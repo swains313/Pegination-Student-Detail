f57d581bbd87b4114fd615f69dffba72137a8d33
saumyaranjan <68143654+swains313@users.noreply.github.com>
2023-05-27 19:28:33 +0530

    Update StudentRepository.java

--- BACK-END/Student-Detail-Kinara/src/main/java/com/student/kinara/repository/StudentRepository.java
+++ BACK-END/Student-Detail-Kinara/src/main/java/com/student/kinara/repository/StudentRepository.java
@@ -25,6 +25,9 @@ public interface StudentRepository extends MongoRepository<Student, String>{
 	 List<Student> findByNameContaining(String query);
 	
 
+	Page<Student> findByNameContainingIgnoreCase(String query, Pageable pageable);
+	
+
 
 	
 
